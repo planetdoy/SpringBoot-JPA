8cfca0d34842ab55baae5e9891707e471e1d6189
planetdoy <doydoit@gmail.com>
2021-08-20 19:00:00 +0900

    feat :엔티티 클래스 개발
    실전! 스프링 부트와 JPA 활용1 - 도메인 분석 설계
    요구사항 분석 후 도메인 모델과 테이블 설계를 바탕으로 엔티티 클래스들을 개발합니다.

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //protected
+    //JPA 구현 라이브러리가 객체를 생성할 때 리플랙션 같은 기술을 사용할 수 있도록 지원해야하기 때문이다.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
