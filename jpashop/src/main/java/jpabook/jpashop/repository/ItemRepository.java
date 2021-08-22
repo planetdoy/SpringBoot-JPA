1ce95f43fdf05574b6e90ec43b41a05d12af0fdf
planetdoy <doydoit@gmail.com>
2021-08-22 11:27:46 +0900

    feat : 상품 리포지토리 개발
    실전! 스프링 부트와 JPA 활용1 - 상품 도메인 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    //상품 등록 및 업데이트..?
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    //단일 상품 조회
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    //상품 전체 조회
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
