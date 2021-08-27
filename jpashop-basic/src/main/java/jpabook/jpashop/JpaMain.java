83a595bfdc53256a15e7883ac546de572d1b5156
planetdoy <doydoit@gmail.com>
2021-08-27 16:40:21 +0900

    실전 예제 1 - 요구사항 분석과 기본 매핑
    자바 ORM 표준 JPA 프로그래밍 - 기본편 : 엔티티 매핑
    데이터 중심 설계의 문제점

---
+++ jpashop-basic/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        //정석
+        try {
+
+            Order order = em.find(Order.class, 1L);
+
+            //데이터 중심 설계의 문제점
+            //Member member = em.find(Member.class, order.getMemberId());
+            Member member = order.getMember();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        //close
+        emf.close();
+    }
+}
