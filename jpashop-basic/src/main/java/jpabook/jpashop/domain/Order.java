52da9a9a438065f65ede84388e66905cd6fede81
planetdoy <doydoit@gmail.com>
2021-08-27 18:51:28 +0900

    실전 예제 2 - 연관관계 매핑 시작
    자바 ORM 표준 JPA 프로그래밍 - 기본편 : 연관 관계 매핑 기초
    단방향 연관 관계 설정

--- jpashop-basic/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop-basic/src/main/java/jpabook/jpashop/domain/Order.java
@@ -12,10 +12,14 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
-
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus orderStatus;
 
     public Member getMember() {
         return member;
@@ -25,25 +29,12 @@ public class Order {
         this.member = member;
     }
 
-    private LocalDateTime orderDate;
-
-    @Enumerated(value = EnumType.STRING)
-    private OrderStatus orderStatus;
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
     }
 
     public LocalDateTime getOrderDate() {
--- jpashop-basic/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop-basic/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,9 +1,6 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class OrderItem {
@@ -13,15 +10,33 @@ public class OrderItem {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @Column(name = "ORDER_ID")
-    private Long orderId;
-
-    @Column(name = "ITEM_ID")
-    private long itemId;
-
     private int orderPrice;
 
     private int count;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 
     public Long getId() {
         return id;
@@ -29,22 +44,6 @@ public class OrderItem {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(long itemId) {
-        this.itemId = itemId;
     }
 
     public int getOrderPrice() {
