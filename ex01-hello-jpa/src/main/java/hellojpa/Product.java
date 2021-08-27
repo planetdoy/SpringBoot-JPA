4d72b75c2bd8a3fad8a1ec23ed0dc2967724a926
planetdoy <doydoit@gmail.com>
2021-08-27 19:59:10 +0900

    다대다 [N:M]
    자바 ORM 표준 JPA 프로그래밍 - 기본편 : 다양한 연관관계 매핑
    다대다 단방향 매핑

---
+++ ex01-hello-jpa/src/main/java/hellojpa/Product.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
