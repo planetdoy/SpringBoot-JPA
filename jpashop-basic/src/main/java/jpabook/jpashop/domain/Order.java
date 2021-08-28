1073191ec55265c448a1b40fb3801a6374ecc8cd
planetdoy <doydoit@gmail.com>
2021-08-28 13:33:56 +0900

    실전 예제 5 - 연관관계 관리
    자바 ORM 표준 JPA 프로그래밍 - 기본편 : 프록시와 연관관계 관리
    글로벌 패치 전략 설정
    즉시 로딩 -> 지연 로딩

--- jpashop-basic/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop-basic/src/main/java/jpabook/jpashop/domain/Order.java
@@ -13,7 +13,7 @@ public class Order extends BaseEntity{
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
